fd8c5f1ffaa3864f81c5bc6c9a2cb455e9a184e6
Fleur26 <lera.hoholeva@yandex.ru>
2025-01-04 00:38:24 +0400

    add formik

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,88 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Formik, Form, ErrorMessage, Field } from "formik";
+import * as Yup from "yup";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const initialValues = {
+    email: "",
+    username: "",
+    password: "",
+  };
+  const onSubmit = (values) => {
+    console.log("onSubmit", values);
+  };
 
+  const validationSchema = Yup.object({
+    email: Yup.string()
+      .required("Email is required")
+      .email("Invalid email adress"),
+    password: Yup.string().required("Password is required"),
+    username: Yup.string().required("Username is required"),
+  });
+  const formik = useFormik({
+     initialValues: {
+       email: "",
+       username: "",
+     password: "",
+    },
+     onSubmit: (values) => {
+     console.log("onSubmit", values);
+     },
+    validationSchema: Yup.object({
+      email: Yup.string()
+       .required("Email is required")
+        .email("Invalid email adress"),
+      password: Yup.string().required("Password is required"),
+     username: Yup.string().required("Username is required"),
+     }),
+   validate: (values) => {
+    const errors = {};
+     if (!values.email) {
+       errors.email = "Email is required";
+     }
+     if (!values.username) {
+       errors.username = "Username is required";
+     }
+     if (!values.password) {
+       errors.password = "Password is required";
+     }
+    return errors;
+   },
+   });
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+      <h1>Log in</h1>
+      <Formik
+        initialValues={initialValues}
+        onSubmit={onSubmit}
+        validationSchema={validationSchema}
+      >
+        {() => (
+          <Form>
+            <div className="field">
+              <Field name="email" placeholder="Email" />
 
-export default App
+              <div className="error">
+                <ErrorMessage name="email" component="span" />
+              </div>
+            </div>
+            <div className="field">
+              <Field name="username" placeholder="Username" />
+              <div className="error">
+                <ErrorMessage name="username" component="span" />
+              </div>
+            </div>
+            <div className="field">
+              <Field name="password" placeholder="Password" type="password" />
+              <div className="error">
+                <ErrorMessage name="password" component="span" />
+              </div>
+            </div>
+            <button type="submit">Submit</button>
+          </Form>
+        )}
+      </Formik>
+    </div>
+  );
+};
+
+export default App;
