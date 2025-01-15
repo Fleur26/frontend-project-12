0e801f16e7fc762c76ba31506a0845e04473f2aa
Fleur26 <lera.hoholeva@yandex.ru>
2025-01-15 20:15:03 +0400

    add slices of messages

---
+++ frontend/src/slices/messages.js
@@ -0,0 +1,10 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { actions as channelsActions } from './channelsjs'; 
+
+const initialState = {
+  messages: [],
+};
+
+const messagesSlice = createSlice({
+
+});
