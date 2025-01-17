911933229c9c9567717025b7585ccbd5106bf426
Fleur26 <lera.hoholeva@yandex.ru>
2025-01-17 19:29:26 +0400

    add sockets in index.js

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,8 +1,35 @@
-import { createRoot } from 'react-dom/client';
 import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { configureStore } from '@reduxjs/toolkit';
+import { io } from 'socket.io-client';
+import App from './components/App';
+import rootReducer from './slices';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
 
-import Component from './components/App.jsx';
+// store
+const store = configureStore({
+  reducer: rootReducer,
+});
 
-const container = document.getElementById('container');
-const root = createRoot(container);
-root.render(<Component />);
+// Создание подключения к WebSocket
+const socket = io(); // Адрес сервера WebSocket
+export const SocketContext = React.createContext(socket); // Экспорт контекста
+
+// Проверка наличия элемента root перед рендерингом
+const rootElement = document.getElementById('root');
+if (!rootElement) {
+  throw new Error('Root element not found');
+}
+
+
+ReactDOM.createRoot(rootElement).render(
+  <Provider store={store}>
+    <SocketContext.Provider value={socket}> {/* Передаём сокет через контекст */}
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </SocketContext.Provider>
+  </Provider>
+);
