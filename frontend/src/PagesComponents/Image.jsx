98ecd7d0467e26b643e88c08beef76b974dccd9a
Fleur26 <lera.hoholeva@yandex.ru>
2025-01-29 21:26:56 +0400

    final polish

--- frontend/src/PagesComponents/Image.jsx
+++
@@ -1,7 +0,0 @@
-const Image = ({ image, alt }) => (
-    <div className="col-12 col-md-6 d-flex align-items-center justify-content-center">
-      <img src={image} className="rounded-circle" alt={alt} width={300} />
-    </div>
-  );
-  
-  export default Image;
