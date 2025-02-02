1b77506854fca279bca600b4f9a10b04411a64da
Fleur26 <lera.hoholeva@yandex.ru>
2025-02-02 18:58:34 +0400

    return localhost

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,11 +8,11 @@ export default defineConfig({
     proxy: {
       // Проксируем запросы к API
       '/api': {
-        target: 'http://100.20.92.101:5001',
+        target: 'http://localhost:5001',
       },
       // Проксируем WebSocket соединения
       '/socket.io': {
-        target: 'ws://100.20.92.101:5001',
+        target: 'ws://localhost:5001',
         ws: true,
         rewriteWsOrigin: true,
       },
