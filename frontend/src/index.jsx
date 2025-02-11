1355f399a50bf872a58d793c4c156699e517d796
Fleur26 <lera.hoholeva@yandex.ru>
2025-02-11 12:45:59 +0400

    delete gitignore, elete import bootstrap

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,6 +1,5 @@
 import ReactDOM from 'react-dom/client';
 import init from './init';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 const app = async () => {
   const root = ReactDOM.createRoot(document.getElementById('root'));
